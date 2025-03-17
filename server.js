f610cb19275c68fd90d6c48548f58ac28dd38726
DeTekgai <infinitekodes001@gmail.com>
2025-03-17 19:17:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["*", "https://cryphorizon.com", "https://cryphorizon.netlify.app"],
+    origin: process.env.CORS_SERVER
     credentials: true,
   })
 );
